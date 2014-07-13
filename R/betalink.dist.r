3160b98f62a9691be690735d7da20cd439529127
Matt Barbour <barbour@zoology.ubc.ca>
2014-07-13 11:41:55 -0700

    updated betalink.dist function
    Modified so that betalink.dist returns distance matrices for upper and
    lower trophic levels in the bipartite matrix. Also added an option to
    accommodate quantitative metrics and an option for rectangular output
    of distance matrices.

--- R/betalink.dist.r
+++ R/betalink.dist.r
@@ -1,22 +1,32 @@
-betalink.dist = function(W,...){
+# Modified so that betalink.dist returns distance matrices for upper and lower trophic levels in the bipartite matrix.  
+# added an option to accommodate quantitative metrics
+# Also added an option for rectangular output of distance matrices
+
+betalink.dist = function(W,triangular=F,bf="jaccard"){
 	dWN = matrix(NA,ncol=length(W),nrow=length(W))
 	colnames(dWN) = names(W)
 	rownames(dWN) = names(W)
+	diag(dWN)=0
 	dOS = dWN
-	dS = dWN
+	dS_all.taxa = dWN
+	dS_upper = dWN
+	dS_lower = dWN
 	dST = dWN
 	dContrib = dWN
-	for(i in c(1:(length(W)-1))){
-		for(j in c((i+1):(length(W)))){
-			partition = betalink(W[[i]],W[[j]],...)
-			dWN[j,i]		= partition$WN
-			dOS[j,i]		= partition$OS
-			dS[j,i]			= partition$S
-			dST[j,i]		= partition$ST
-			dContrib[j,i]	= partition$contrib
+	for(i in 1:(length(W)-1)){
+		for(j in (i+1):length(W)){
+		  index=cbind(c(j,i),c(i,j))
+			partition = betalink(W[[i]],W[[j]],bf)
+			dWN[index]		= partition$WN
+			dOS[index]		= partition$OS
+			dS_all.taxa[index]  = partition$S
+			dS_upper[index]   = partition$U
+			dS_lower[index]    = partition$L
+			dST[index]		= partition$ST
+			dContrib[index]	= partition$contrib
 		}
 	}
-	distances = list(WN=dWN, OS=dOS, S=dS, ST=dST, contrib=dContrib)
-	distances = lapply(distances,as.dist)
+	distances = list(WN=dWN, OS=dOS, S_all.taxa=dS_all.taxa, S_upper=dS_upper ,S_lower=dS_lower, ST=dST, contrib=dContrib)
+	if(triangular==T) distances = lapply(distances,as.dist)
 	return(distances)
 }
